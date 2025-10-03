a24c883a4ac3dd1c7ae1c69c923ace2e9ecee1d8
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-10-03 14:10:58 -0300

    Bump version: 0.1.1 → 0.2.0 [ci skip]

--- src/msilib/__init__.py
+++ src/msilib/__init__.py
@@ -7,7 +7,7 @@ import re
 import string
 import sys
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 AMD64 = "AMD64" in sys.version
 # Keep msilib.Win64 around to preserve backwards compatibility.
