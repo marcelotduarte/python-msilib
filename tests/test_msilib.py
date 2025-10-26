2c2c84d65cf878c1349268beffb8be49e561cf10
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-10-26 09:44:48 -0300

    Use ruff check

--- tests/test_msilib.py
+++ tests/test_msilib.py
@@ -7,7 +7,7 @@ schema = pytest.importorskip("msilib.schema", reason="Windows tests")
 
 
 @pytest.fixture
-def db(tmp_path):  # -> msilib.Database
+def db(tmp_path) -> msilib.Database:
     path = tmp_path / "test.msi"
     db = msilib.init_database(
         path.as_posix(), schema, "Python Tests", "product_code", "1.0", "PSF"
@@ -16,7 +16,7 @@ def db(tmp_path):  # -> msilib.Database
     db.Close()
 
 
-def test_view_fetch_returns_none(db):
+def test_view_fetch_returns_none(db) -> None:
     properties = []
     view = db.OpenView("SELECT Property, Value FROM Property")
     view.Execute(None)
@@ -35,7 +35,7 @@ def test_view_fetch_returns_none(db):
     ]
 
 
-def test_view_non_ascii(db):
+def test_view_non_ascii(db) -> None:
     view = db.OpenView("SELECT 'ß-розпад' FROM Property")
     view.Execute(None)
     record = view.Fetch()
@@ -43,7 +43,7 @@ def test_view_non_ascii(db):
     view.Close()
 
 
-def test_summaryinfo_getproperty_issue1104(db):
+def test_summaryinfo_getproperty_issue1104(db) -> None:
     try:
         sum_info = db.GetSummaryInformation(99)
         title = sum_info.GetProperty(msilib.PID_TITLE)
@@ -65,42 +65,42 @@ def test_summaryinfo_getproperty_issue1104(db):
         sum_info = None
 
 
-def test_database_open_failed():
+def test_database_open_failed() -> None:
     with pytest.raises(msilib.MSIError) as exc:
         msilib.OpenDatabase("non-existent.msi", msilib.MSIDBOPEN_READONLY)
     assert exc.match("open failed")
 
 
-def test_database_create_failed(tmp_path):
+def test_database_create_failed(tmp_path) -> None:
     with pytest.raises(msilib.MSIError) as exc:
         msilib.OpenDatabase(tmp_path.as_posix(), msilib.MSIDBOPEN_CREATE)
     assert exc.match("create failed")
 
 
-def test_get_property_vt_empty(db):
+def test_get_property_vt_empty(db) -> None:
     summary = db.GetSummaryInformation(0)
     assert summary.GetProperty(msilib.PID_SECURITY) is None
 
 
-def test_directory_start_component_keyfile(db, tmp_path):
+def test_directory_start_component_keyfile(db, tmp_path) -> None:
     try:
         feature = msilib.Feature(db, 0, "Feature", "A feature", "Python")
         cab = msilib.CAB("CAB")
-        dir = msilib.Directory(
+        directory = msilib.Directory(
             db, cab, None, tmp_path, "TARGETDIR", "SourceDir", 0
         )
-        dir.start_component(None, feature, None, "keyfile")
+        directory.start_component(None, feature, None, "keyfile")
     finally:
-        msilib._directories.clear
+        msilib._directories.clear()  # noqa: SLF001
 
 
-def test_getproperty_uninitialized_var(db):
+def test_getproperty_uninitialized_var(db) -> None:
     si = db.GetSummaryInformation(0)
     with pytest.raises(msilib.MSIError):
         si.GetProperty(-1)
 
 
-def test_FCICreate(tmp_path):
+def test_FCICreate(tmp_path) -> None:
     filepath = tmp_path / "test.txt"
     cabpath = tmp_path / "test.cab"
     filepath.touch()
@@ -116,20 +116,20 @@ underscore.
 """
 
 
-def test_make_id_no_change_required():
+def test_make_id_no_change_required() -> None:
     assert msilib.make_id("short") == "short"
     assert msilib.make_id("nochangerequired") == "nochangerequired"
     assert msilib.make_id("one.dot") == "one.dot"
     assert msilib.make_id("_") == "_"
     assert msilib.make_id("a") == "a"
-    # assert msilib.make_id("") == ""
+    # assert msilib.make_id("") == ""  # noqa: ERA001
 
 
-def test_make_id_invalid_first_char():
+def test_make_id_invalid_first_char() -> None:
     assert msilib.make_id("9.short") == "_9.short"
     assert msilib.make_id(".short") == "_.short"
 
 
-def test_make_id_invalid_any_char():
+def test_make_id_invalid_any_char() -> None:
     assert msilib.make_id(".s\x82ort") == "_.s_ort"
     assert msilib.make_id(".s\x82o?*+rt") == "_.s_o___rt"
